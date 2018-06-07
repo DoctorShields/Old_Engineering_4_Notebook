cc3116ebdfc2f473ce46b190402d929dab6552f2
doctorshields <matthew.shields@charlottesvilleschools.org>
2018-06-07 13:49:23 +0000

    a pretty full web app

--- Python/Flask/example04/app.py
+++ Python/Flask/example04/app.py
@@ -7,8 +7,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index:
+def index():
 	return render_template('index.html')
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=80, debug=True)
